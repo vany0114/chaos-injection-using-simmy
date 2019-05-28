6f9536064989a6784d1dea35edf4797c563e4688
Geovanny Alzate Sandoval <vany0114@hotmail.com>
2019-05-27 23:12:41 -0500

    Added support to use in memory cache in dev environment.

---
+++ src/Application/Duber.Chaos.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Duber.Chaos.API.Infrastructure.Repository;
+using Duber.Infrastructure.Chaos;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Duber.Chaos.API.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
+        {
+            // swagger configuration
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Title = "Duber.Chaos HTTP API",
+                    Version = "v1",
+                    Description = "The Duber Chaos Service HTTP API",
+                    TermsOfService = "Terms Of Service"
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetEntryAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddDistributedCache(this IServiceCollection services, IConfiguration configuration, IHostingEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                services.AddMemoryCache();
+                services.AddDistributedMemoryCache();
+            }
+            else
+            {
+                services.AddDistributedRedisCache(option =>
+                {
+                    option.Configuration = configuration.GetValue<string>("ConnectionString");
+                    option.InstanceName = "master";
+                });
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection AddRepository(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IChaosRepository, ChaosRepository>();
+            services.Configure<GeneralChaosSetting>(configuration.GetSection("GeneralChaosSetting"));
+            return services;
+        }
+    }
+}
--- src/Application/Duber.Chaos.API/Startup.cs
+++ src/Application/Duber.Chaos.API/Startup.cs
@@ -1,58 +1,33 @@
-﻿using Duber.Chaos.API.Infrastructure.Repository;
-using Duber.Infrastructure.Chaos;
+﻿using Duber.Chaos.API.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.IO;
-using System.Reflection;
 
 namespace Duber.Chaos.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
         }
 
         public IConfiguration Configuration { get; }
+
+        public IHostingEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services
+                .AddCustomSwagger()
+                .AddDistributedCache(Configuration, Environment)
                 .AddApplicationInsightsTelemetry(Configuration)
+                .AddRepository(Configuration)
                 .AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            // swagger configuration
-            services.AddSwaggerGen(options =>
-            {
-                options.DescribeAllEnumsAsStrings();
-                options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
-                {
-                    Title = "Duber.Chaos HTTP API",
-                    Version = "v1",
-                    Description = "The Duber Chaos Service HTTP API",
-                    TermsOfService = "Terms Of Service"
-                });
-
-                // Set the comments path for the Swagger JSON and UI.
-                var xmlFile = $"{Assembly.GetEntryAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                options.IncludeXmlComments(xmlPath);
-            });
-
-            services.AddDistributedRedisCache(option =>
-            {
-                option.Configuration = Configuration.GetValue<string>("ConnectionString");
-                option.InstanceName = "master";
-            });
-
-            services.AddSingleton<IChaosRepository, ChaosRepository>();
-            services.Configure<GeneralChaosSetting>(Configuration.GetSection("GeneralChaosSetting"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
